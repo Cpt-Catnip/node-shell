a3647b863337d93a211d08cd388e87b051399b54
Aleks Mitrovic <mitrovic.aleksandar@outlook.com>
2020-06-19 13:05:04 -0400

    ls and pwd commands added; created map to store all commands; added try/catch to handle unkown commands

--- bash.js
+++ bash.js
@@ -1,17 +1,18 @@
+const map = {
+  pwd: require("./pwd"),
+  ls: require("./ls")
+};
+
 // Output a prompt
-process.stdout.write('prompt > ');
+process.stdout.write("prompt > ");
 
 // The stdin 'data' event fires after a user types in a line
-process.stdin.on('data', (data) => {
+process.stdin.on("data", (data) => {
   const cmd = data.toString().trim(); // remove newline
-
-  // check for pwd
-  if (cmd === 'pwd') {
-    // process.stdout.write(__dirname);
-    process.stdout.write(process.cwd());
-  } else {
-    process.stdout.write('You typed: ' + cmd);
+  try {
+    map[cmd]();
+  } catch {
+    process.stdout.write("\nUnknown Command " + cmd);
+    process.stdout.write("\nprompt > ");
   }
-
-  process.stdout.write('\nprompt > ');
 });
